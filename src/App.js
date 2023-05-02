f27997fa49182603cf3f7b5e4fd12da1c05b2d33
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-05-02 20:59:25 +0530

    First comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Header } from './components/Header.js';
+import { Balance } from './components/Balance.js';
+import { IncomeExpense } from './components/IncomeExpense.js';
+import { TransactionList } from './components/TransactionList.js';
+import { AddTransaction } from './components/AddTransaction.js';
+import { GlobalProvider } from './context/GlobalState.js';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header />
+     <div className="container">
+      <Balance />
+      <IncomeExpense />
+      <TransactionList />
+      <AddTransaction />
+      <GlobalProvider />
+     </div>
     </div>
-  );
+  )
 }
 
 export default App;
